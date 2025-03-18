ca5395f11d2ad6a35a04e9de1649183a13040fad
Ten-T-Tonmoy <stu01754@gmail.com>
2025-03-18 23:59:14 +0600

    enough of bs

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+import { userServcies } from "../services/user.services";
+
+
+export const userController={
+    getAllusers: async(req:Request,res:Response)=>{
+        try
+    }
+}
---
+++ src/services/user.services.ts
@@ -0,0 +1,15 @@
+import prisma from "../utils/database";
+
+interface createUser {
+  name: string;
+  age: number;
+}
+
+interface updateUser {
+  name: string;
+  age: number;
+}
+
+export const userServcies={
+    
+}
